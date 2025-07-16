ff63e0657c623a2b5ef7ccce71c261f8430d6f3e
Martin Bustos <bustos.martin@outlook.com>
2025-07-16 10:04:53 -0300

    agregar busqueda en db de usuarios por id

---
+++ modelos/modeloUsuarios.js
@@ -0,0 +1,18 @@
+const db = require('../config/db.js');
+
+class ModeloUsuarios {
+	constructor(){};
+
+	/*
+	* Metodo para buscar usuario por su id
+	*/
+	buscarPorId(id=null, callback) {
+		if (id === null) return new Error('id no especificada');
+		if (typeof id !== 'number') return new Error('tipo de dato invalido');
+		db.query('SELECT id, username FROM users WHERE id = ?', id, (err, result) => {
+			if (err) return callback(err);
+			const user = result[0];
+			return callback(null, user);
+		});
+	}
+}	
