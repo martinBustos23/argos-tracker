6ec21d8cfe22048bb0b2fd1cf16aecca595d1f0f
Martin Bustos <bustos.martin@outlook.com>
2025-08-11 18:36:20 -0300

    agregar router de trackers

---
+++ src/routers/api/trackerRouter.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import { authToken } from '../../utils.js';
+
+export default function createTrackerRouter(trackerController, userController) {
+  const router = express.Router();
+  router.use(authToken);
+
+  router.post('/trackers', async (req, res) => {
+    try {
+      // console.log(Object.getOwnPropertyNames(req.body));
+      const user = await userController.findByID(req.user);
+      if (!user.admin) return res.status(401).json({ error: 'No autorizado' });
+      const tracker = await trackerController.createTracker(req.body);
+
+      console.log('-- Crear tracker --');
+      console.table(tracker); //
+
+      res.status(201).json(tracker);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.get('/trackers', async (req, res) => {
+    try {
+      const trackers = await trackerController.getAll();
+
+      console.log('-- Encontrar tracker/s --');
+      console.table(trackers); //test
+
+      res.status(200).json(trackers);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.get('/trackers/:id', async (req, res) => {
+    try {
+      const tracker = await trackerController.findByID(req.params.id);
+
+      console.log('-- Encontrar traker (id) --');
+      console.table(tracker); //test
+
+      res.status(200).json(tracker);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.put('/trackers/:id', async (req, res) => {
+    try {
+      const user = await userController.findByID(req.user);
+      if (!user.admin) return res.status(401).json({ error: 'No autorizado' });
+      const updatedTracker = await trackerController.updateTracker(req.params.id, req.body);
+
+      console.log('-- Actualizar tracker --');
+      console.log(req.params.id);
+      console.log(req.body);
+      console.table(updatedTracker);
+
+      res.status(200).json(updatedTracker);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  router.delete('/trackers/:id', async (req, res) => {
+    try {
+      const user = await userController.findByID(req.user);
+      if (!user.admin) return res.status(401).json({ error: 'No autorizado' });
+      const result = await trackerController.delete(req.params.id);
+
+      console.log('-- Eliminar tracker --');
+      console.log(req.params.id);
+      console.table(result);
+
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  return router;
+}
