25e828fcf96740323b83c3b8cb56d36afe11bffb
Martin Bustos <bustos.martin@outlook.com>
2025-08-11 11:57:14 -0300

    crear modelo para trackers

---
+++ src/model/trackerModel.js
@@ -0,0 +1,33 @@
+export default class TrackerModel {
+    #dao;
+    constructor(dao) {
+        this.#dao = dao;
+    }
+
+    async createTracker(tracker) {
+        const exists = await this.#dao.findById(tracker.id);
+        if (!exists) throw new Error('Tracker ya existe');
+        return await this.#dao.create(tracker);
+    }
+
+    async findTrackerById(id) {
+        return await this.#dao.findById(id);
+    }
+
+    async updateTracker(tracker) {
+        const exists = await this.#dao.findById(tracker.id);
+        if (!exists) throw new Error('Tracker no existe');
+        return await this.#dao.update(tracker);
+    }
+
+    async deleteTracker(tracker) {
+        const exists = await this.#dao.findById(tracker.id);
+        if (!exists) throw new Error('Tracker no existe');
+        return await this.#dao.delete(tracker.id);
+    }
+
+    async getAllTrackers() {
+        const trackers = await this.#dao.getAll();
+        return trackers;
+    }
+}
