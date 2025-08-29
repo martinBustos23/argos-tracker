be624ac557060dba0f84b15e84078ca3b1b58563
Martin Bustos <bustos.martin@outlook.com>
2025-08-29 09:08:26 -0300

    agregar username a account-btn

--- src/routers/views/dashboardRouter.js
+++ src/routers/views/dashboardRouter.js
@@ -1,26 +1,34 @@
 import express from 'express';
-import { authToken } from '../../utils.js';
+import { authToken, getUserFromToken } from '../../utils.js';
 
 export default (userController) => {
   const router = express.Router();
   router.use(authToken);
 
   router.get('/', (req, res) => {
-    res.render('./dashboard/general');
+    const token = req.cookies.authorization;
+    const username = getUserFromToken(token);
+    res.render('./dashboard/general', { username });
   });
 
   router.get('/devices', (req, res) => {
-    res.render('./dashboard/devices');
+    const token = req.cookies.authorization;
+    const username = getUserFromToken(token);
+    res.render('./dashboard/devices', { username });
   });
 
   router.get('/config', (req, res) => {
-    res.render('./dashboard/config');
+    const token = req.cookies.authorization;
+    const username = getUserFromToken(token);
+    res.render('./dashboard/config', { username });
   });
 
   router.get('/users', async (req, res) => {
     try {
       const users = await userController.getAll();
-      res.render('./dashboard/users', { users });
+      const token = req.cookies.authorization;
+      const username = getUserFromToken(token);
+      res.render('./dashboard/users', { username, users });
     } catch (error) {
       res.status(500).send('Error al obtener usuarios');
     }
