b19356cb0247d686751c5d22229b01241459dd09
Martin Bustos <61627748+martinBustos23@users.noreply.github.com>
2025-10-01 00:50:52 +0100

    eliminar atributos falsy

--- src/dto/userDTO.js
+++ src/dto/userDTO.js
@@ -1,11 +1,11 @@
 export default class UserDTO {
   constructor(user) {
-    this.id = user.id;
-    this.username = user.username;
-    this.password = user.password;
-    this.image = user.image;
-    this.admin = user.admin ? true : false;
-    this.active = user.active ? true : false;
+    if (user.id) this.id = user.id;
+    if (user.username) this.username = user.username;
+    if (user.password) this.password = user.password;
+    if (user.image) this.image = user.image || 'default';
+    if (user.admin) this.admin = user.admin ? true : false;
+    if (user.active) this.active = user.active ? true : false;
     if (user.lastLogin) this.lastLogin = new Date(user.lastLogin + 'UTC');
     if (user.timeout) this.timeout = new Date(user.timeout + 'UTC');
   }
